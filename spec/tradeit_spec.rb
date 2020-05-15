1a346e7ea32a9185d923322e476c4b180e9d7055
Ren <renlester@myfave.com>
2020-05-15 08:50:38 +0800

    :white_check_mark: update base specs

--- spec/tradeit_spec.rb
+++ spec/tradeit_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Tradeit do
   it "has a version number" do
     expect(Tradeit::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
